97782ff87d8ddcfef3892999382836e03ae2482f
atharv1205 <atharvadudhe125@gmail.com>
2025-04-02 14:07:41 +0530

    fixed the get API endpoint

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,45 +3,46 @@ import { users } from "../data.js";
 
 const router = express.Router();
 
+// get Api endpoint
 router.get('/', (req, res) => {
     res.json(users);
 });
 
-router.post("/", (req,res) => {
-    const { name, username, email, games, achievements, highScore } = req.body;
-    if (!name || !username || !email) {
-        return res.status(400).json({ message: "Name, username and email are required!" });
-    }
-    const newUser = {
-        name,
-        username,
-        email,
-        games: games || [],
-        achievements: achievements || [],
-        highScore: highScore || 0
-    };
-    users.push(newUser);
-    res.status(201).json(newUser);
-})
+// router.post("/", (req,res) => {
+//     const { name, username, email, games, achievements, highScore } = req.body;
+//     if (!name || !username || !email) {
+//         return res.status(400).json({ message: "Name, username and email are required!" });
+//     }
+//     const newUser = {
+//         name,
+//         username,
+//         email,
+//         games: games || [],
+//         achievements: achievements || [],
+//         highScore: highScore || 0
+//     };
+//     users.push(newUser);
+//     res.status(201).json(newUser);
+// })
 
-router.put("/:username", (req, res) => {
-    const { username } = req.params;
-    const { name, email, games, achievements, highScore } = req.body;
-    const userIndex = users.findIndex(user => user.username == username);
-    if(userIndex == -1){
-        return res.status(400).json({ "message" : "User not found!" })
-    }
-    const updateUser = {
-        ...users[userIndex],
-        name: name || users[userIndex].name,
-        email: email || users[userIndex].email,
-        games: games || users[userIndex].games,
-        achievements: achievements || users[userIndex].achievements,
-        highScore: (highScore !== undefined && highScore !== null) ? highScore : users[userIndex].highScore
+// router.put("/:username", (req, res) => {
+//     const { username } = req.params;
+//     const { name, email, games, achievements, highScore } = req.body;
+//     const userIndex = users.findIndex(user => user.username == username);
+//     if(userIndex == -1){
+//         return res.status(400).json({ "message" : "User not found!" })
+//     }
+//     const updateUser = {
+//         ...users[userIndex],
+//         name: name || users[userIndex].name,
+//         email: email || users[userIndex].email,
+//         games: games || users[userIndex].games,
+//         achievements: achievements || users[userIndex].achievements,
+//         highScore: (highScore !== undefined && highScore !== null) ? highScore : users[userIndex].highScore
 
-    };
-    users[userIndex] = updateUser;
-    res.json(updateUser)
-})
+//     };
+//     users[userIndex] = updateUser;
+//     res.json(updateUser)
+// })
 
 export default router;
